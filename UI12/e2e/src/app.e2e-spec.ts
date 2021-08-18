c2dbcde931b9ec9c4e9fb553ee54c08ed7e34220
Oliver Weimar-Drese <oliver@weimar-drese.de>
2021-08-18 18:55:57 +0200

    change of UI11 project with UI12

--- UI12/e2e/src/app.e2e-spec.ts
+++ UI12/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('UI11 app is running!');
+    expect(await page.getTitleText()).toEqual('UI12 app is running!');
   });
 
   afterEach(async () => {
--- UI12/karma.conf.js
+++ UI12/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/UI11'),
+      dir: require('path').join(__dirname, './coverage/UI12'),
       subdir: '.',
       reporters: [
         { type: 'html' },
