f55db6a2b4b52ad6221bc8256208e21e5455336b
Oliver Weimar-Drese <oliver@weimar-drese.de>
2022-11-05 16:35:29 +0000

    added support for seifenblase

--- UI14/src/app/app-routing.module.ts
+++ UI14/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { OrbitComponent } from './nonportal/orbit/orbit.component';
 import { Uni1993Component } from './nonportal/uni1993/uni1993.component';
 import { UnitopiaComponent } from './nonportal/unitopia/unitopia.component';
 import { DualComponent } from './nonportal/dual/dual.component';
+import { SeifenblaseComponent } from './nonportal/seifenblase/seifenblase.component';
 
 const routes: Routes = [
   { path: 'orbit',   /* canActivate: [NonPortalGuard], */component: OrbitComponent },
   { path: 'uni1993',   /* canActivate: [NonPortalGuard], */component: Uni1993Component },
+  { path: 'seifenblase',   /* canActivate: [NonPortalGuard], */component: SeifenblaseComponent },
   // { path: 'dual',   /* canActivate: [NonPortalGuard], */component: DualComponent },
   /*{ path: 'portal',  canActivate: [PortalGuard],component: UnitopiaComponent },*/
   { path: '',       /* canActivate: [NonPortalGuard], */component: UnitopiaComponent,  pathMatch: 'full' },
