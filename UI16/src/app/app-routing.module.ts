d7efdbfdad9946b5fb90be35bc90f1336515ad9a
Oliver Weimar-Drese <myonara1@googlemail.com>
2023-11-08 22:21:37 +0100

    cherry?

--- UI16/src/app/app-routing.module.ts
+++ UI16/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Inject, NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ROUTES } from '@angular/router';
 import { OrbitComponent } from './nonportal/orbit/orbit.component';
 import { Uni1993Component } from './nonportal/uni1993/uni1993.component';
@@ -88,64 +88,6 @@ const standardRoutes: Routes = [{ path: '**', redirectTo: '/' }];
       deps: [MudConfigService],
       multi: true,
     },
-  ],
-  providers: [
-    {
-      provide: ROUTES,
-      useFactory: (mudcfg:MudConfigService) => {
-        let routes: Routes = [];
-        let mroutes = mudcfg.data.routes;
-        let count = 0;
-        let rootFlag = false;
-        Object.keys(mroutes).forEach( (key:string) => {
-          if (mroutes.hasOwnProperty(key) && key.startsWith("/")) {
-            const path = key.substring(1);
-            let component : any = undefined;
-            switch (mroutes[key]) {
-              case 'unitopia':
-                component = UnitopiaComponent;
-                break;
-              case 'orbit':
-                component = OrbitComponent;
-                break;
-              case 'uni1993':
-                component = Uni1993Component;
-                break;
-              case 'seifenblase':
-                component = SeifenblaseComponent;
-                break;
-              default:
-                console.error("unknown mud route:",key,mroutes[key]);
-                break;
-            }
-            if (typeof component !== 'undefined') {
-              if (key === '/') {
-                routes.push({
-                  path,
-                  component,
-                  pathMatch: 'full'
-                });
-                rootFlag = true;
-                } else {
-                routes.push({
-                  path,
-                  component
-                })
-              }
-            }
-          }
-        });
-        if (!rootFlag) {
-          console.error("No root node!");
-        }
-        return [
-          ...routes,
-          ...standardRoutes
-        ];
-      },
-      deps:[MudConfigService],
-      multi: true
-    }
   ],
   exports: [RouterModule],
 })
