29d80be0b7e301f7c3b8402a3145bed087e550f4
myst <anon@b.cd>
2024-04-29 23:57:59 +0200

    feat(client): remove i18n
    fixes Remove unused features mystiker/webmud3#4

--- UI17/src/app/settings/color-settings/color-settings.component.ts
+++ UI17/src/app/settings/color-settings/color-settings.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 import { ColorSettings } from 'src/app/mud/color-settings';
-import { ReadLanguageService } from 'src/app/read-language.service';
 
 @Component({
   selector: 'app-color-settings',
@@ -16,7 +15,6 @@ export class ColorSettingsComponent implements OnInit {
   cbThis: any; // paththrough
 
   constructor(
-    public i18n: ReadLanguageService,
     public ref: DynamicDialogRef,
     public config: DynamicDialogConfig,
   ) {}
