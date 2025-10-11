b4a173b42088dc3dba0f88c704ed938b91344dec
myst <chr.kellner@live.de>
2025-10-12 00:02:45 +0200

    feat(frontend): removed custom ANSI handling

--- frontend/src/app/features/ansi/models/escape-sequences.ts
+++
@@ -1,9 +0,0 @@
-export const ESCAPE_SEQUENCES = {
-  CLEAR_SCREEN: 'e[He[J',
-  FG256: '38;5;',
-  BG256: '48;5;',
-  FG_RGB: '39;2;',
-  BG_RGB: '49;2;',
-  VALID: /^[0-9;A-Za-z]$/,
-  ENDCHAR: /^[A-Za-z]$/,
-} as const;
--- frontend/src/app/shared/index.ts
+++ frontend/src/app/shared/index.ts
@@ -1,3 +1,2 @@
 export { WithRequired } from './types/with-required';
-export { wordWrap } from './utils/word-wrap';
 export { SecureString, isSecureString } from './types/secure-string';
